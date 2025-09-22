82ca527f9c4fefc2fb94894435ba2b0c4108d043
gorandonfun-ui <233602525+gorandomfun-ui@users.noreply.github.com>
2025-09-22 11:44:15 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -176,7 +176,7 @@ export default function HomePage() {
       {/* Centre */}
       <section
         ref={heroRef}
-        className="flex flex-col items-center px-4 flex-1 justify-center pb-[72px] md:pb-16 lg:pb-[200px]"
+        className="flex flex-col items-center px-4 flex-1 justify-center mb-[72px] md:mb-16 lg:mb-[200px]"
         style={{ minHeight: `calc(100vh - ${HEADER_H + FOOTER_H + AD_H}px)` }}
       >
         <LogoAnimated
